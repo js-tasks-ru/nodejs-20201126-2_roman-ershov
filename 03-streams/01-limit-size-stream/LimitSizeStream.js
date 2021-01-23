98ade70e91add786ccd4540dfbc10870a926b4d4
Roman Ershov <romma4893@gmail.com>
2021-01-23 11:39:38 +0300

    streams

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,9 +4,22 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.limit = options.limit;
+    this.total = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    this.total += chunk.length;
+
+    if (this.total > this.limit) {
+      let error = new LimitExceededError();
+
+      this.emit('error', error);
+      callback(error, chunk);
+    } else {
+      callback(null, chunk);
+    }
 
   }
 }
