ec506b2ecf66c2e86127208dd8fd2e36090d0f88
ro.ershov <ro.ershov@tensor.ru>
2021-02-27 16:54:06 +0300

    line split stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -1,15 +1,34 @@
-const stream = require('stream');
-const os = require('os');
+const {Transform} = require('stream');
+const {EOL} = require('os');
 
-class LineSplitStream extends stream.Transform {
+class LineSplitStream extends Transform {
   constructor(options) {
     super(options);
+
+    this._storageLines = [];
   }
 
   _transform(chunk, encoding, callback) {
+    const lines = chunk.toString().split(EOL);
+
+    this._storageLines[0] = (this._storageLines[0] || '') + lines[0];
+
+    if (lines.length === 1) {
+      callback();
+    } else {
+      callback(null, this._storageLines[0]);
+      this._storageLines = lines.slice(1);
+    }
   }
 
   _flush(callback) {
+    this._storageLines.forEach( (line) => {
+      if (line) {
+        this.push(line);
+      }
+    });
+    this._storageLines = [];
+    callback();
   }
 }
 
