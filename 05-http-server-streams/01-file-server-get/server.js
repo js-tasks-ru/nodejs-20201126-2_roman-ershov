5f37bef2b33704edd9aee5ba152b2911bd2ab4fc
ro.ershov <ro.ershov@tensor.ru>
2021-03-01 18:43:42 +0300

    server get

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -1,19 +1,30 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const {createReadStream} = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'GET':
+      let readStream = createReadStream(filepath);
 
+      readStream.on('error', () => {
+        if (pathname.includes('/')) {
+          res.statusCode = 400;
+        } else {
+          res.statusCode = 404;
+        }
+
+        res.end();
+      });
+
+      readStream.pipe(res);
       break;
-
     default:
       res.statusCode = 501;
       res.end('Not implemented');
